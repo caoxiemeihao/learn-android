f44e5699e7319868dedea65895aa143985748c4e
草鞋没号 <308487730@qq.com>
2019-03-31 18:26:25 +0800

    19-03-31 Vertical RecyclerView.

---
+++ RecyclerViewTest/app/src/main/java/com/atom/recyclerviewtest/FruitAdapter.java
@@ -0,0 +1,52 @@
+package com.atom.recyclerviewtest;
+
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> {
+
+    private List<Fruit> mFruitList;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView fruitImage;
+        TextView fruitName;
+
+        public ViewHolder(View view) {
+            super(view);
+            fruitImage = view.findViewById(R.id.fruit_image);
+            fruitName = view.findViewById(R.id.fruit_name);
+        }
+    }
+
+    public FruitAdapter(List<Fruit> fruitList) {
+        mFruitList = fruitList;
+        Log.d("-----------------", mFruitList.size() + "");
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewTye) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fruit_item, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Fruit fruit = mFruitList.get(position);
+        holder.fruitImage.setImageResource(fruit.getImageId());
+        holder.fruitName.setText(fruit.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mFruitList.size();
+    }
+}
